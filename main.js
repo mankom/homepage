91a9ea6b31ed019e2a60dac8ec0a95ec32397f3d
Marek Mańko <merak.manko@gmail.com>
2019-06-28 12:51:34 +0200

    js: add content to object using js

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-consol.log('siema');
+console.log('siema');
 const name = 'Marek';
 const age = 41;
 console.log(name);
 console.log(age);
-console.log(`Nazywam się ${name} i mam ${age} lat.`)
+console.log(`Nazywam się ${name} i mam ${age} lat.`);
+const jsFun = document.querySelector('.aboutMe__paragraph--js');
+console.log(jsFun); //wyświetlenie wszystkiego co znajdyje się między znacznikami oznaczonmymi klasą aboutMe__paragraph--js
+jsFun.innerHTML= 'Czasem próbuję swoich sił w C#.';
+console.log(jsFun.innerHTML);
